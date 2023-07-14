2253c575d1332e036b38e2774f2227774fc46041
Ronald Mjonono <mjay@M-JAY.local>
2023-07-14 21:22:57 +0200

    setup webpack for project

--- webpack-to-do-list/src/index.js
+++ webpack-to-do-list/src/index.js
@@ -1,13 +1,12 @@
+
 import _ from "lodash";
 import "./style.css";
-
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
+  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
+ element.classList.add("hello");
   return element;
 }
 
--- webpack-to-do-list/webpack.config.js
+++ webpack-to-do-list/webpack.config.js
@@ -1,26 +1,43 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "development",
+  devServer: {
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      template: "./src/index.html",
     }),
+    new NodePolyfillPlugin(),
   ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
     ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      child_process: false,
+      fs: false,
+      os: false,
+      path: false,
+    },
   },
 };
