9cb720b54351950c9b5019dd50b278e95596c25e
Ronald Mjonono <mjay@M-JAY.local>
2023-07-20 00:52:43 +0200

    add-interactive-feature

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-import './style.css';
-import reIcon from './recycle.png';
-import tdIcon from './3dotd.png';
-import adIcon from './enter.png';
+import "./style.css";
+import reIcon from "./recycle.png";
+import tdIcon from "./3dotd.png";
+import adIcon from "./enter.png";
+import { updateStatus, clearCompleted } from "./status.js";
 
 // DOM elements
-const inHead = document.querySelector('.div-heading');
-const ulList = document.getElementById('to-do-list');
-const clearall = document.querySelector('.clear-all');
-const ifempty = document.querySelector('.ifempty');
-const addlistdiv = document.querySelector('.list-adding');
+const inHead = document.querySelector(".div-heading");
+const ulList = document.getElementById("to-do-list");
+const clearall = document.querySelector(".clear-all");
+const ifempty = document.querySelector(".ifempty");
+const addlistdiv = document.querySelector(".list-adding");
 
 // Create the app heading image
 const reicon = new Image();
 reicon.src = reIcon;
-reicon.className = 'reload-image';
-reicon.setAttribute('alt', 'reload-icon');
+reicon.className = "reload-image";
+reicon.setAttribute("alt", "reload-icon");
 inHead.appendChild(reicon);
 
 // Create input element to add tasks
-const addlist = document.createElement('input');
-addlist.setAttribute('type', 'text');
-addlist.setAttribute('placeholder', 'Add to your list ...');
-addlist.setAttribute('id', 'add-new-list');
+const addlist = document.createElement("input");
+addlist.setAttribute("type", "text");
+addlist.setAttribute("placeholder", "Add to your list ...");
+addlist.setAttribute("id", "add-new-list");
 
 // Create button for adding tasks
-const addiconbutton = document.createElement('button');
-addiconbutton.type = 'button';
-addiconbutton.className = 'addbutton';
+const addiconbutton = document.createElement("button");
+addiconbutton.type = "button";
+addiconbutton.className = "addbutton";
 
 // Create image for add button
 const addicon = new Image();
 addicon.src = adIcon;
-addicon.setAttribute('alt', 'addicon');
-addicon.className = 'addicon';
+addicon.setAttribute("alt", "addicon");
+addicon.className = "addicon";
 
 // Append elements to the add list div
 addiconbutton.appendChild(addicon);
@@ -40,14 +41,14 @@ addlistdiv.appendChild(addlist);
 addlistdiv.appendChild(addiconbutton);
 
 // Array of objects (empty at start)
-let tasks = JSON.parse(localStorage.getItem('todolist')) || [];
+let tasks = JSON.parse(localStorage.getItem("todolist")) || [];
 
 // Helper function to reset task indices after deletions
 function setIndex() {
   tasks.forEach((task, index) => {
     task.id = index + 1;
   });
-  localStorage.setItem('todolist', JSON.stringify(tasks));
+  localStorage.setItem("todolist", JSON.stringify(tasks));
 }
 
 class Create {
@@ -57,35 +58,35 @@ class Create {
 
   // Method to create, update, and delete tasks
   createTodo() {
-    const eachtask = document.createElement('li');
-    eachtask.className = 'each-task';
+    const eachtask = document.createElement("li");
+    eachtask.className = "each-task";
 
-    const checktag = document.createElement('div');
-    checktag.className = 'check-tag';
+    const checktag = document.createElement("div");
+    checktag.className = "check-tag";
 
-    const cbox = document.createElement('input');
-    cbox.setAttribute('class', 'task-check');
-    cbox.setAttribute('type', 'checkbox');
+    const cbox = document.createElement("input");
+    cbox.setAttribute("class", "task-check");
+    cbox.setAttribute("type", "checkbox");
     cbox.checked = this.newtask.complete;
 
     if (this.newtask.complete) {
-      eachtask.classList.add('checked');
+      eachtask.classList.add("checked");
     }
 
-    const tbox = document.createElement('input');
-    tbox.setAttribute('type', 'text');
-    tbox.setAttribute('class', 'task-desc');
+    const tbox = document.createElement("input");
+    tbox.setAttribute("type", "text");
+    tbox.setAttribute("class", "task-desc");
     tbox.value = this.newtask.description;
 
-    const bbin = document.createElement('button');
-    bbin.className = 'delbin';
-    bbin.setAttribute('type', 'button');
-    bbin.setAttribute('id', this.newtask.id);
+    const bbin = document.createElement("button");
+    bbin.className = "delbin";
+    bbin.setAttribute("type", "button");
+    bbin.setAttribute("id", this.newtask.id);
 
     const threedots = new Image();
-    threedots.className = 'three-dots';
+    threedots.className = "three-dots";
     threedots.src = tdIcon;
-    threedots.setAttribute('alt', 'threedots');
+    threedots.setAttribute("alt", "threedots");
 
     checktag.appendChild(cbox);
     checktag.appendChild(tbox);
@@ -95,42 +96,42 @@ class Create {
     eachtask.appendChild(bbin);
     ulList.appendChild(eachtask);
 
-    cbox.addEventListener('change', () => {
+    cbox.addEventListener("change", () => {
       this.newtask.complete = cbox.checked;
 
       if (this.newtask.complete) {
-        eachtask.classList.add('checked');
-        eachtask.style.background = '#f4f5Cf';
-        eachtask.style.opacity = '0.5';
+        eachtask.classList.add("checked");
+        eachtask.style.background = "#f4f5Cf";
+        eachtask.style.opacity = "0.5";
       } else {
-        eachtask.classList.remove('checked');
-        eachtask.style.background = 'none';
-        eachtask.style.opacity = '1';
+        eachtask.classList.remove("checked");
+        eachtask.style.background = "none";
+        eachtask.style.opacity = "1";
       }
 
-      localStorage.setItem('todolist', JSON.stringify(tasks));
+      updateStatus(this.newtask.id, this.newtask.complete, tasks);
     });
 
-    tbox.addEventListener('input', () => {
+    tbox.addEventListener("input", () => {
       this.newtask.description = tbox.value;
     });
 
-    tbox.addEventListener('blur', () => {
-      tbox.setAttribute('disabled', '');
-      eachtask.style.background = '#eff9fd';
-      localStorage.setItem('todolist', JSON.stringify(tasks));
+    tbox.addEventListener("blur", () => {
+      tbox.setAttribute("disabled", "");
+      eachtask.style.background = "#eff9fd";
+      localStorage.setItem("todolist", JSON.stringify(tasks));
     });
 
-    checktag.addEventListener('click', () => {
-      tbox.removeAttribute('disabled');
+    checktag.addEventListener("click", () => {
+      tbox.removeAttribute("disabled");
       tbox.focus();
     });
 
-    threedots.addEventListener('click', () => {
+    threedots.addEventListener("click", () => {
       tasks = tasks.filter((task) => task.id !== this.newtask.id);
       eachtask.remove();
       setIndex();
-      localStorage.setItem('todolist', JSON.stringify(tasks));
+      localStorage.setItem("todolist", JSON.stringify(tasks));
     });
 
     return {
@@ -151,59 +152,55 @@ function addTask(val) {
   tasks.push(newtask);
   const n = new Create(newtask);
   n.createTodo();
-  localStorage.setItem('todolist', JSON.stringify(tasks));
+  localStorage.setItem("todolist", JSON.stringify(tasks));
 }
 
-addlistdiv.addEventListener('click', (e) => {
+addlistdiv.addEventListener("click", (e) => {
   e.preventDefault();
 
-  addiconbutton.addEventListener('click', () => {
-    if (addlist.value !== '') {
+  addiconbutton.addEventListener("click", () => {
+    if (addlist.value !== "") {
       addTask(addlist.value);
-      addlist.value = '';
+      addlist.value = "";
     }
   });
 });
 
 function displayList() {
-  if (localStorage.getItem('todolist')) {
-    tasks = JSON.parse(localStorage.getItem('todolist'));
+  if (localStorage.getItem("todolist")) {
+    tasks = JSON.parse(localStorage.getItem("todolist"));
     tasks.forEach((task) => {
       const n = new Create(task);
       const { eachtask } = n.createTodo();
       if (task.complete) {
-        eachtask.classList.add('checked');
-        eachtask.style.background = '#f4f5Cf';
-        eachtask.style.opacity = '0.5';
+        eachtask.classList.add("checked");
+        eachtask.style.background = "#f4f5Cf";
+        eachtask.style.opacity = "0.5";
       } else {
-        eachtask.classList.remove('checked');
-        eachtask.style.background = 'none';
-        eachtask.style.opacity = '1';
+        eachtask.classList.remove("checked");
+        eachtask.style.background = "none";
+        eachtask.style.opacity = "1";
       }
     });
   }
 }
 
-document.addEventListener('DOMContentLoaded', displayList);
+document.addEventListener("DOMContentLoaded", displayList);
 
 const clearCheckedTasks = () => {
-  tasks = tasks.filter((task) => !task.complete);
-  setIndex();
-  localStorage.setItem('todolist', JSON.stringify(tasks));
-  ulList.innerHTML = '';
-  displayList();
+  clearCompleted(tasks, setIndex, ulList, displayList);
 };
 
-clearall.addEventListener('click', clearCheckedTasks);
+clearall.addEventListener("click", clearCheckedTasks);
 
-addiconbutton.addEventListener('click', () => {
-  if (addlist.value === '') {
+addiconbutton.addEventListener("click", () => {
+  if (addlist.value === "") {
     ifempty.innerHTML = `
       <p>Enter a task to add to the list for today</p>
       <span class='smiley'>&#128512;</span>
     `;
     setTimeout(() => {
-      ifempty.innerHTML = '';
+      ifempty.innerHTML = "";
     }, 4000);
   }
 });
